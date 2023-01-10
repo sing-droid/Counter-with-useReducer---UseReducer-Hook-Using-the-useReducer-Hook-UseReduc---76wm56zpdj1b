b43048abe48b8e6a08e798f3ec770664bb575278
SONU SINGH <sonusingh82342@gmail.com>
2023-01-11 00:49:53 +0530

    changes

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,18 @@
 const counterReducer = (state,action) =>{
-
+    switch (action.type) {
+        case 'INCREMENT':
+          return {
+            ...state,
+            [action.name]: state[action.name] + 1,
+          };
+        case 'DECREMENT':
+          return {
+            ...state,
+            [action.name]: state[action.name] - 1,
+          };
+        default:
+          return state;
+      }
 }
 
 export {counterReducer}
